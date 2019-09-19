903e6fced0265d4b6ccd33f45d6f4bd9ea5a76e5
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-19 23:36:22 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,25 @@
+class Genre
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+  
+  def artists
+    song_artists = []
+    songs.each {|song| song_artists << song.artist}
+    song_artists
+  end
+  
+end
