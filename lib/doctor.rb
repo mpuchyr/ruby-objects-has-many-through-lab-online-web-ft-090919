1e2d69c0000b05676e4d743b53ab41b60e14dcf4
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-19 22:27:28 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,26 @@
+class Doctor
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(patient, date)
+    appt = Appointment.new(patient, self, date)
+  end
+  
+  def appointments
+    Appointment.select {|appt| appt.doctor == self}
+  end
+  
+  def patients
+    doc_appts = []
+    appointments.each {|appt| doc_appts << appt.patient}
+    doc_appts
+  end
+  
+end
