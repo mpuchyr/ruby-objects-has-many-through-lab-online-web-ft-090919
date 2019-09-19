bea82bedfab3fe4ccc7ce8a20f080ad143268944
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-19 22:05:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,7 +25,7 @@ class Artist
     artist_genres = []
     Song.all.each do |song|
       if song.artist == self
-        artist_genres << song.genre.name
+        artist_genres << song.genre
       end
     end
     artist_genres
